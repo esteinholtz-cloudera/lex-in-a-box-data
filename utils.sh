c2876659d289710bb2783f5db3d10a8a0a728201
Erik <esteinholtz@cloudera.com>
2023-11-15 22:53:28 +0100

    exported fucntions from utils.sh

--- utils.sh
+++ utils.sh
@@ -1,27 +1,48 @@
 # the following is a library of useful snippets, it is
 # meant to be used in a controlled way, line-by-line - not as a wholesale script to just run through
+# you need to have the right prereqs, be in the right directory, etc etc
 
 # extract the text from the aws transcript files
-for f in *.json; do cat $f|jq '.results.transcripts.[0].transcript' > $f.txt;done
+extract() {
+    for f in *.json; do cat $f | jq '.results.transcripts.[0].transcript' >$f.txt; done
+}
 
 # change name of all files from *.json.txt to *.txt
-for f in *.json.txt;do mv $f `echo $f|cut -d"." -f1,3`;done
+unjson() {
+    for f in *.json.txt; do mv $f $(echo $f | cut -d"." -f1,3); done
+}
 
 # split the text files into 1k chunks
-cd split 
-for f in ../*.txt; do mkdir $f.dir; split -b1k $f; mv * $f.dir; done
-cd ..
+chunk() {
+    cd split
+    for f in ../*.txt; do
+        mkdir $f.dir
+        split -b1k $f
+        mv * $f.dir
+    done
+    cd ..
+}
 
 # insert speaker name in the beginning of each file:
 # we make the first file (xaa) exempt, since we're sure Lex is the speaker there
 
-for f in *.dir;do echo $f|cut -d"." -f1|cut -d"_" -f3,4|tr "_" " "|read -r spkr;cd $d
-mv xaa ..
-for f in *
-do echo $spkr: > $f.txt
-cat $f >> $f.txt
-rm $f
-done
-mv ../xaa ./xaa.txt
-cd ..
-done
+insert_speaker() {
+    for d in *.dir; do
+        echo $d | cut -d"." -f1 | cut -d"_" -f3,4 | tr "_" " " | read -r spkr
+        cd $d
+        mv xaa ..
+        for f in *; do
+            echo $spkr: >$f.txt
+            echo $spkr
+            cat $f >>$f.txt
+            rm $f
+        done
+        mv ../xaa ./xaa.txt
+        cd ..
+    done
+}
+
+# check
+check() {
+    more *.dir/xa[ab].txt
+}
