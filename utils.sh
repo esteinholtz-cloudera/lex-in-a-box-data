7d3b25f5031562db903f4c8ae4d523b048ec6f67
Erik <esteinholtz@cloudera.com>
2023-11-15 21:37:29 +0100

    updated utils.sh

--- utils.sh
+++ utils.sh
@@ -1,12 +1,7 @@
-# the following is meant to be used in a controlled way, line-by-line - not as a wholesale script to just run through
-# extract the transcription only from the Amazon transcribe json
+# the following is a library of useful snippets, it is
+# meant to be used in a controlled way, line-by-line - not as a wholesale script to just run through
 
-# in order to split files into 1k chunks;
-# the resulting files will be located in directories named <file>.dir
-# TODO: output directories should be in a parallel directory to not mess up the original
-# TODO: output files to be named *.txt
-for f in ../*.txt; do mkdir $f.dir; split -b1k $f; mv * $f.dir; done
-
+# extract the text from the aws transcript files
 for f in *.json; do cat $f|jq '.results.transcripts.[0].transcript' > $f.txt;done
 
 # change name of all files from *.json.txt to *.txt
@@ -18,7 +13,7 @@ for f in ../*.txt; do mkdir $f.dir; split -b1k $f; mv * $f.dir; done
 cd ..
 
 # insert speaker name in the beginning of each file:
-# we make xaa exempt, since we're sure Lex is the speaker there
+# we make the first file (xaa) exempt, since we're sure Lex is the speaker there
 for d in *.dir; do cd $d
 mv xaa ..
 echo $PWD|cut -d"/" -f6|cut -d"." -f1|cut -d"_" -f3,4|tr "_" " "|read -r spkr 
